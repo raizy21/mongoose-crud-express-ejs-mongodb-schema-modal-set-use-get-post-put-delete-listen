b5c2c0e4ad0a9b8fdff7d8291cf2560e993fc6bc
Razvan Andrei <chiper_raizy@yahoo.com>
2024-12-05 09:37:22 +0100

    mongo db connection

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/farmStand")
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!");
+    console.log(err);
+  });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
