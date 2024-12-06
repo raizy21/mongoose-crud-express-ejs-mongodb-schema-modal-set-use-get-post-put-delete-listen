87b7d3e25b937d7e45dbbec24b8035c7cdf2847c
Razvan Andrei <chiper_raizy@yahoo.com>
2024-12-06 08:15:56 +0100

    Get one product (using ID)

--- index.js
+++ index.js
@@ -20,9 +20,16 @@ app.set("view engine", "ejs");
 
 app.get("/products", async (req, res) => {
   const products = await Product.find();
-
   res.render("products/index", { products });
 });
+
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
+});
+
 
 app.listen(3000, () => {
   console.log("APP IS LISTENING ON PORT 3000!");
